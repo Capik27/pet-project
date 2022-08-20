429a824313c2149ec208e28e7000881fd1576e4a
Konstantin <kaplan.sk8@mail.ru>
2022-08-20 20:30:42 +0400

    add project files

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+import { NavBar } from "./components/NavBar";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <NavBar />
+    </BrowserRouter>
+  );
+}
